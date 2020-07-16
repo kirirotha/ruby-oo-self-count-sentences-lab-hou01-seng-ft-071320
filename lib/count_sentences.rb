86fcf783fccc52436db1295cdac288cfd0919767
Kiri Um <kirirotha@gmail.com>
2020-07-16 12:40:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,25 @@ class String
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      true
+    else
+      false
+    end
+    
   end
 
   def count_sentences
+    sentence_count = self.split(".").count
 
+    
   end
 end
